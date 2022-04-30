8b5b401a72086bc437030fe8b6b29ab9e303da06
ador <rashedulhaque111@gmail.com>
2022-05-01 03:05:21 +0600

    firebase installed

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAlMHbDO2ccCfCdNWqiFexz-GLLtWhYd9Y",
+    authDomain: "affinity-79d3a.firebaseapp.com",
+    projectId: "affinity-79d3a",
+    storageBucket: "affinity-79d3a.appspot.com",
+    messagingSenderId: "1072131570264",
+    appId: "1:1072131570264:web:bc65e2374a9ad4c4a6a82d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
