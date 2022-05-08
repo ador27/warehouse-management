9a2c86af5bc9087c5c58cbfaa4b0e4247e882d1b
ador <rashedulhaque111@gmail.com>
2022-05-08 15:50:53 +0600

    clothing types updated

--- src/Components/Home/ClothingType/ClothingType.js
+++ src/Components/Home/ClothingType/ClothingType.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FloralWhite from '../../../Images/images/IMG_0353.JPG';
-import PlatinumBlack from '../../../Images/images/IMG_0368.JPG';
-import PartyMarron from '../../../Images/images/IMG_0543.JPG';
+import Hoodie from '../../../Images/images/Hoodie.jpg';
+import Shirt from '../../../Images/images/shirt.jpg'
 
 const ClothingType = () => {
     return (
@@ -19,18 +19,18 @@ const ClothingType = () => {
                 </a>
                 <a href="www.facebook.com/theaffinity.bd">
                     <div className='max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
-                        <img className='object-cover h-64 w-96 ...' src={PlatinumBlack} alt="" />
+                        <img className='object-cover h-64 w-96 ...' src={Shirt} alt="" />
                         <div className='p-5'>
-                            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Platinum Black Punjabi EID COLLECTION 2022</h5>
+                            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Platinum Shirt EID COLLECTION 2022</h5>
                             <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>Best Premium Collection of AFFINITY in this Eid of 2022. It's a pure cotton with china silk which looks more gorgeous.</p>
                         </div>
                     </div>
                 </a>
                 <a href="www.facebook.com/theaffinity.bd">
                     <div className='max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
-                        <img className='object-cover h-64 w-96 ...' src={PartyMarron} alt="" />
+                        <img className='object-cover h-64 w-96 ...' src={Hoodie} alt="" />
                         <div className='p-5'>
-                            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Party Marron Punjabi EID COLLECTION 2022</h5>
+                            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Premium Hoodie EID COLLECTION 2022</h5>
                             <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>Best Premium Collection of AFFINITY in this Eid of 2022. It's a pure cotton with china silk which looks more gorgeous.</p>
                         </div>
                     </div>
